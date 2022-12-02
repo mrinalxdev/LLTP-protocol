d89f8b524e630963806a42cb1251e3a87500c361
Hi_Mrinal <mrinalbitsat@gmail.com>
2022-12-02 10:43:44 +0530

    Development Phase

--- travel-space/src/components/Header/Header.jsx
+++ travel-space/src/components/Header/Header.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Autocomplete} from '@react-google-maps/api'
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
+
+import useStyles from './style'
+
 
 export const Header = () => {
+  const classes = useStyles()
+
   return (
     <>
-        <h1>This is Header</h1>
+        <AppBar position='static'>
+          <Toolbar >
+            <Typography variant = 'h4'>
+              Travel Space
+            </Typography>
+
+            <Autocomplete>
+              <div className = {classes.search}>
+                 <div className= {classes.searchIcon}>
+                  <SearchIcon />
+                 </div>
+                 <InputBase placeholder='Search ...' classes = {{root : classes.inputRoot, input : classes.inputInput}}/>
+              </div>
+            </Autocomplete>
+          </Toolbar>
+        </AppBar>
     
     </>
   )
