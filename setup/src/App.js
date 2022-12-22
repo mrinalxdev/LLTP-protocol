c7631c3ea3fda3016915b1c39c480f15ac222833
Hi_Mrinal <mrinalbitsat@gmail.com>
2022-12-22 13:58:27 +0530

    Added : Header Completed

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { CssBaseline, Grid} from '@material-ui/core'
+
+import Header from './components/Header/Header'
+import List from './components/List/List'
+import Maps from './components/Maps/Maps'
+import PlaceDetails from './components/PlaceDetails/PlaceDetails'
+
 
 const App = () => {
   return (
-    <div>
-      <h1>This is TravelSpace</h1>
-    </div>
+    <>
+    <CssBaseline />
+    <Header />
+    <Grid container spacing={3} style={{ width: '100%'}}>
+        <Grid item xs={12} md={4}>
+            <List />
+        </Grid>
+        <Grid item xs={12} md={4}>
+            <Maps />
+        </Grid>
+    </Grid>
+    </>
   )
 }
 
