a023b13c0ce5c60f23e9ef0f1bf7a6f052a49117
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-01-29 15:42:09 +0530

    Update : Added Data

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-import  notesAPI form "./notesAPI.js"
+import notesAPI from "./notesAPI.js"
+
+notesAPI.deleteNote(739148)
+
+console.log(notesAPI.getAllNotes())
--- js/notesAPI.js
+++ js/notesAPI.js
@@ -1,3 +1,35 @@
 export default class notesAPI {
-    
+    static getAllNotes(){
+        const notes = JSON.parse(localStorage.getItem("noteapp-notes") || "[]")
+
+        return notes.sort((a, b) => {
+            return new Date(a.updated) > new Date(b.updated) ? -1 : 1;
+        })
+    }
+
+    static saveNote(noteToSave){
+        const notes = notesAPI.getAllNotes()
+        const existing = notes.find(notes => note.id == noteToSave.id)
+
+        if (existing){
+            existing.title = noteToSave.title
+            existing.body = noteToSave.body
+            existing.updated = new Date().toISOString()
+        }else {
+            noteToSave.id = Math.floor(Math.random() * 1000000)
+            noteToSave.updated = new Date().toISOString();
+            notes.push(noteToSave);
+        }
+
+
+        localStorage.setItem("notesapp-notes", JSON.stringify(notes))
+    }
+
+    static deleteNote(id){
+        const notes = notesAPI.getAllNotes()
+        const newNotes = notes.filter(note => notes.id != id)
+
+        localStorage.setItem("notesapp-notes", JSON.stringify(newNotes))
+
+    }
 }
